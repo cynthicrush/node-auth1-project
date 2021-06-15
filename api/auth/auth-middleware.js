4838fd904515c1553ae061510e03f60b864d33a8
Cynthia Canada <77354197+cynthicrush@users.noreply.github.com>
2021-06-14 21:53:51 -0400

    Homework changes

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,9 +8,13 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
-}
+function restricted(res, req, next) {
+  if(req.session.user) {
+    next()
+  }else {
+   next({ status: 401, message: 'You shall not pass!' })
+  }
+ }
 
 /*
   If the username in req.body already exists in the database
@@ -18,8 +24,16 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  try{
+    const user = await User.findBy({ username: req.body.username })
+    if(!user.length) {
+      next()
+    }
+    else next({'message': 'Username taken', status: 422})
+  } catch(err) {
+    next(err)
+  }
 }
 
 /*
@@ -30,8 +44,17 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  try{
+    const user = await User.findBy({ username: req.body.username })
+    if(users.length) {
+      req.user = users[0]
+      next()
+    }
+    else next({'message': 'Invalid credentials', status: 401})
+  } catch(err) {
+    next(err)
+  }
 }
 
 /*
@@ -42,8 +65,18 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if(!req.body.password || req.body.password.length < 3) {
+    next({message: 'Password must be longer than 3 chars', status: 422})
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,13 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 
+const router = require('express').Router()
+const User = require('../users/users-model')
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require('./auth-middleware')
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +32,15 @@
   }
  */
 
+  router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8)
+    User.add({ username, password: hash })
+      .then(saved => {
+        res.status(201).json(saved)
+      })
+      .catch(next)
+  })
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +58,15 @@
   }
  */
 
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const { password } = req.body
+    if(bcrypt.compareSync(password, req.user.password)) {
+      req.session.user = req.user
+      req.json({ message: `Welcome ${req.user.username}`})
+    }else {
+      next({ status: 401, message: 'Invalid credentials' })
+    }
+  })
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +84,19 @@
   }
  */
 
+  router.post('/logout', (req, res, next) => {
+    if(req.session.user) {
+      req.session.destroy(err => {
+        if(err) {
+          next(err)
+        }else{
+          res.json({message: 'logged out'})
+        }
+      })
+    }else {
+      res.json({message: 'no session'})
+    }
+  })
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
